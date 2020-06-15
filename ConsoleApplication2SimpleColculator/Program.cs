babce670ca2221e709cf311e379ae8ac20c36895
M1ke0ris <66957632+M1ke0ris@users.noreply.github.com>
2020-06-15 17:13:23 +0300

    Add files via upload

---
+++ ConsoleApplication2SimpleColculator/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApplication2SimpleColculator
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int firstNumber;
+            int secondNumber;
+            string sign;
+            int answer;
+            
+            Console.WriteLine("Welcome at Misha's calculator! Please enter first number");
+            firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter a sign(+ , - , * , /)");
+            sign = Console.ReadLine();
+            
+            Console.WriteLine("Please enter second nubmer");
+            secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            if (sign == "*")
+            {
+                answer = firstNumber * secondNumber;
+                Console.WriteLine($"{firstNumber} {sign} {secondNumber} = {answer}");
+            }
+            else if (sign == "-")
+            {
+                answer = firstNumber - secondNumber;
+                Console.WriteLine($"{firstNumber} {sign} {secondNumber} = {answer}");
+            }
+            else if (sign == "+")
+            {
+                answer = firstNumber + secondNumber;
+                Console.WriteLine($"{firstNumber} {sign} {secondNumber} = {answer}");
+            }
+            else if (sign == "/")
+            {
+                answer = firstNumber / secondNumber;
+                Console.WriteLine($"{firstNumber} {sign} {secondNumber} = {answer}");
+            }
+        }
+    }
+}
